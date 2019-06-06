86669116bf75e1447a90d50ab49c28d02d9cd41a
Emily Collins <emtcollins4@gmail.com>
2019-06-06 12:05:49 +1000

    week1

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -9,7 +9,7 @@ from .models import *
 def example_get(request, var_a, var_b):
 	try:
 		returnob = {
-		"data": "%s: %s" %(var_a, var_b),
+		"data": "%s is really silly x %s" %(var_a, var_b),
 		}
 		return JsonResponse(returnob)
 	except Exception as e:
